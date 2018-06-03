0346c5bb58a12f2b70620532fc25cc29b439ca3c
Yael <yaeldau@gmail.com>
2018-06-04 01:48:53 +0300

    create submit page

--- Service.js
+++ Service.js
@@ -1,7 +1,8 @@
 let id = 2;  // TODO: change to 0
 // let forms = [];  //[Form] -> [{id, name, fields]] -> {id: [name, {label, name, type}]}
-let forms = {0: {form_name: 'f1', fields: [{label: 'first name', name: 'fname', type: 'text'}], submissions: 5}, 1: {form_name: 'f2', fields: [{label: 'first name', name: 'fname', type: 'text'}], submissions: 0}};
-
+let forms = {0: {form_name: 'f1', fields: [{label: 'first name', name: 'fname', type: 'text'},{label: 'age', name: 'age', type: 'number'}
+            ,{label: 'eye color', name: 'eye_color', type: 'color'}], submissions: 5}, 1: {form_name: 'f2', fields: [{label: 'first name', name: 'fname', type: 'text'}], submissions: 0}};
+let answers = {};
 function get_id(){
     return id++;
 }
@@ -24,7 +25,20 @@ module.exports = {
         forms[new_form.id] = new_form;
         return true;
     },
-
+    add_submit: function (form_id, form_answers){
+        let new_submit = {};
+        form_answers.forEach(function(pair) {
+            new_submit[pair.question] = pair.ans;
+        });
+        if (form_id in answers) {
+            answers[form_id].push(new_submit);
+        }
+        else{
+            answers[form_id] = [];
+            answers[form_id].push(new_submit);
+        }
+        forms[form_id].submissions++;
+    },
 
 
 };
--- routes/submit.js
+++ routes/submit.js
@@ -2,11 +2,33 @@
 var express = require('express');
 var router = express.Router();
 const service = require('../Service');
+var bodyParser = require('body-parser')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 /* GET submit page. */
 router.get('/:form_id', function(req, res, next) {
-    res.render('submit', { title: 'Express', form_id: req.params.form_id});
+    let form = service.get_form(req.params.form_id);
+    res.render('submit', {form_id: req.params.form_id, form_name: form.form_name, fields: form.fields});
+});
+
+router.post('/:form_id', urlencodedParser, function(req, res) {
+    console.log("form submited");
+    console.log(req.body);
+    let pairs = [];
+    let fields = service.get_form(req.params.form_id).fields;
+    fields.forEach(function(field){
+        let pair = {};
+        pair.question = field.label;
+        // let field_id = 'input_' + field.name;
+        // pair.ans = req.body.field_id;
+        pair.ans = "1";
+        pairs.push(pair);
+    });
+
+    service.add_submit(req.params.form_id, pairs);
+    res.redirect('/thanks');
 });
 
 
